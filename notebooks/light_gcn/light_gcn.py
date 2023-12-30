b147617def7f2946bbbccfdee9dc291f1f8a615c
RomanGrebnev <roman.gr.115@tut.by>
2023-12-30 13:12:52 +0100

    Added train-test split class and examples

---
+++ notebooks/light_gcn/light_gcn.py
@@ -0,0 +1,358 @@
+import numpy as np
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+from torch import torch
+from sklearn import preprocessing
+
+from torch_geometric.data import Data
+from torch_geometric.transforms.to_undirected import ToUndirected
+
+import random
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch import nn, optim, Tensor
+
+from torch_sparse import SparseTensor, matmul
+
+# from torch_geometric.utils import structured_negative_sampling
+from torch_geometric.data import download_url, extract_zip
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.typing import Adj
+from typing import Optional, Tuple, Union
+from torch_geometric.utils.num_nodes import maybe_num_nodes
+
+
+# defines LightGCN model
+class LightGCN(MessagePassing):
+    """LightGCN Model as proposed in https://arxiv.org/abs/2002.02126"""
+
+    def __init__(
+        self, num_users, num_items, embedding_dim=64, K=3, add_self_loops=False
+    ):
+        """Initializes LightGCN Model
+
+        Args:
+            num_users (int): Number of users
+            num_items (int): Number of items
+            embedding_dim (int, optional): Dimensionality of embeddings. Defaults to 8.
+            K (int, optional): Number of message passing layers. Defaults to 3.
+            add_self_loops (bool, optional): Whether to add self loops for message passing. Defaults to False.
+        """
+        super().__init__()
+        self.num_users, self.num_items = num_users, num_items
+        self.embedding_dim, self.K = embedding_dim, K
+        self.add_self_loops = add_self_loops
+
+        self.users_emb = nn.Embedding(
+            num_embeddings=self.num_users, embedding_dim=self.embedding_dim
+        )  # e_u^0
+        self.items_emb = nn.Embedding(
+            num_embeddings=self.num_items, embedding_dim=self.embedding_dim
+        )  # e_i^0
+
+        nn.init.normal_(self.users_emb.weight, std=0.1)
+        nn.init.normal_(self.items_emb.weight, std=0.1)
+
+    def forward(self, edge_index: SparseTensor):
+        """Forward propagation of LightGCN Model.
+
+        Args:
+            edge_index (SparseTensor): adjacency matrix
+
+        Returns:
+            tuple (Tensor): e_u_k, e_u_0, e_i_k, e_i_0
+        """
+        # compute \tilde{A}: symmetrically normalized adjacency matrix
+        edge_index_norm = gcn_norm(edge_index, add_self_loops=self.add_self_loops)
+
+        emb_0 = torch.cat([self.users_emb.weight, self.items_emb.weight])  # E^0
+        embs = [emb_0]
+        emb_k = emb_0
+
+        # multi-scale diffusion
+        for i in range(self.K):
+            emb_k = self.propagate(edge_index_norm, x=emb_k)
+            embs.append(emb_k)
+
+        embs = torch.stack(embs, dim=1)
+        emb_final = torch.mean(embs, dim=1)  # E^K
+
+        users_emb_final, items_emb_final = torch.split(
+            emb_final, [self.num_users, self.num_items]
+        )  # splits into e_u^K and e_i^K
+
+        # returns e_u^K, e_u^0, e_i^K, e_i^0
+        return (
+            users_emb_final,
+            self.users_emb.weight,
+            items_emb_final,
+            self.items_emb.weight,
+        )
+
+    def message(self, x_j: Tensor) -> Tensor:
+        return x_j
+
+    def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
+        # computes \tilde{A} @ x
+        return matmul(adj_t, x)
+
+
+def bpr_loss(
+    users_emb_final,
+    users_emb_0,
+    pos_items_emb_final,
+    pos_items_emb_0,
+    neg_items_emb_final,
+    neg_items_emb_0,
+    lambda_val,
+):
+    """Bayesian Personalized Ranking Loss as described in https://arxiv.org/abs/1205.2618
+
+    Args:
+        users_emb_final (torch.Tensor): e_u_k
+        users_emb_0 (torch.Tensor): e_u_0
+        pos_items_emb_final (torch.Tensor): positive e_i_k
+        pos_items_emb_0 (torch.Tensor): positive e_i_0
+        neg_items_emb_final (torch.Tensor): negative e_i_k
+        neg_items_emb_0 (torch.Tensor): negative e_i_0
+        lambda_val (float): lambda value for regularization loss term
+
+    Returns:
+        torch.Tensor: scalar bpr loss value
+    """
+    reg_loss = lambda_val * (
+        users_emb_0.norm(2).pow(2)
+        + pos_items_emb_0.norm(2).pow(2)
+        + neg_items_emb_0.norm(2).pow(2)
+    )  # L2 loss
+
+    pos_scores = torch.mul(users_emb_final, pos_items_emb_final)
+    pos_scores = torch.sum(pos_scores, dim=-1)  # predicted scores of positive samples
+    neg_scores = torch.mul(users_emb_final, neg_items_emb_final)
+    neg_scores = torch.sum(neg_scores, dim=-1)  # predicted scores of negative samples
+
+    loss = -torch.mean(torch.nn.functional.softplus(pos_scores - neg_scores)) + reg_loss
+    return loss
+
+
+def get_user_positive_items(edge_index):
+    """Generates dictionary of positive items for each user
+
+    Args:
+        edge_index (torch.Tensor): 2 by N list of edges
+
+    Returns:
+        dict: dictionary of positive items for each user
+    """
+    user_pos_items = {}
+    for i in range(edge_index.shape[1]):
+        user = edge_index[0][i].item()
+        item = edge_index[1][i].item()
+        if user not in user_pos_items:
+            user_pos_items[user] = []
+        user_pos_items[user].append(item)
+    return user_pos_items
+
+
+# computes recall@K and precision@K
+def RecallPrecision_ATk(groundTruth, r, k):
+    """Computers recall @ k and precision @ k
+
+    Args:
+        groundTruth (list): list of lists containing highly rated items of each user
+        r (list): list of lists indicating whether each top k item recommended to each user
+            is a top k ground truth item or not
+        k (intg): determines the top k items to compute precision and recall on
+
+    Returns:
+        tuple: recall @ k, precision @ k
+    """
+    num_correct_pred = torch.sum(
+        r, dim=-1
+    )  # number of correctly predicted items per user
+    # number of items liked by each user in the test set
+    user_num_liked = torch.Tensor(
+        [len(groundTruth[i]) for i in range(len(groundTruth))]
+    )
+    recall = torch.mean(num_correct_pred / user_num_liked)
+    precision = torch.mean(num_correct_pred) / k
+    return recall.item(), precision.item()
+
+
+# computes NDCG@K
+def NDCGatK_r(groundTruth, r, k):
+    """Computes Normalized Discounted Cumulative Gain (NDCG) @ k
+
+    Args:
+        groundTruth (list): list of lists containing highly rated items of each user
+        r (list): list of lists indicating whether each top k item recommended to each user
+            is a top k ground truth item or not
+        k (int): determines the top k items to compute ndcg on
+
+    Returns:
+        float: ndcg @ k
+    """
+    assert len(r) == len(groundTruth)
+
+    test_matrix = torch.zeros((len(r), k))
+
+    for i, items in enumerate(groundTruth):
+        length = min(len(items), k)
+        test_matrix[i, :length] = 1
+    max_r = test_matrix
+    idcg = torch.sum(max_r * 1.0 / torch.log2(torch.arange(2, k + 2)), axis=1)
+    dcg = r * (1.0 / torch.log2(torch.arange(2, k + 2)))
+    dcg = torch.sum(dcg, axis=1)
+    idcg[idcg == 0.0] = 1.0
+    ndcg = dcg / idcg
+    ndcg[torch.isnan(ndcg)] = 0.0
+    return torch.mean(ndcg).item()
+
+
+# wrapper function to get evaluation metrics
+def get_metrics(model, edge_index, exclude_edge_indices, k):
+    """Computes the evaluation metrics: recall, precision, and ndcg @ k
+
+    Args:
+        model (LighGCN): lightgcn model
+        edge_index (torch.Tensor): 2 by N list of edges for split to evaluate
+        exclude_edge_indices ([type]): 2 by N list of edges for split to discount from evaluation
+        k (int): determines the top k items to compute metrics on
+
+    Returns:
+        tuple: recall @ k, precision @ k, ndcg @ k
+    """
+    user_embedding = model.users_emb.weight
+    item_embedding = model.items_emb.weight
+
+    # get ratings between every user and item - shape is num users x num movies
+    rating = torch.matmul(user_embedding, item_embedding.T)
+
+    for exclude_edge_index in exclude_edge_indices:
+        # gets all the positive items for each user from the edge index
+        user_pos_items = get_user_positive_items(exclude_edge_index)
+        # get coordinates of all edges to exclude
+        exclude_users = []
+        exclude_items = []
+        for user, items in user_pos_items.items():
+            exclude_users.extend([user] * len(items))
+            exclude_items.extend(items)
+
+        # set ratings of excluded edges to large negative value
+        rating[exclude_users, exclude_items] = -(1 << 10)
+
+    # get the top k recommended items for each user
+    _, top_K_items = torch.topk(rating, k=k)
+
+    # get all unique users in evaluated split
+    users = edge_index[0].unique()
+
+    test_user_pos_items = get_user_positive_items(edge_index)
+
+    # convert test user pos items dictionary into a list
+    test_user_pos_items_list = [test_user_pos_items[user.item()] for user in users]
+
+    # determine the correctness of topk predictions
+    r = []
+    for user in users:
+        ground_truth_items = test_user_pos_items[user.item()]
+        label = list(map(lambda x: x in ground_truth_items, top_K_items[user]))
+        r.append(label)
+    r = torch.Tensor(np.array(r).astype("float"))
+
+    recall, precision = RecallPrecision_ATk(test_user_pos_items_list, r, k)
+    ndcg = NDCGatK_r(test_user_pos_items_list, r, k)
+
+    return recall, precision, ndcg
+
+
+# wrapper function to evaluate model
+def evaluation(
+    model, edge_index, sparse_edge_index, exclude_edge_indices, k, lambda_val
+):
+    """Evaluates model loss and metrics including recall, precision, ndcg @ k
+
+    Args:
+        model (LighGCN): lightgcn model
+        edge_index (torch.Tensor): 2 by N list of edges for split to evaluate
+        sparse_edge_index (sparseTensor): sparse adjacency matrix for split to evaluate
+        exclude_edge_indices ([type]): 2 by N list of edges for split to discount from evaluation
+        k (int): determines the top k items to compute metrics on
+        lambda_val (float): determines lambda for bpr loss
+
+    Returns:
+        tuple: bpr loss, recall @ k, precision @ k, ndcg @ k
+    """
+    # get embeddings
+    users_emb_final, users_emb_0, items_emb_final, items_emb_0 = model.forward(
+        sparse_edge_index
+    )
+    edges = structured_negative_sampling(edge_index, contains_neg_self_loops=False)
+    user_indices, pos_item_indices, neg_item_indices = edges[0], edges[1], edges[2]
+    users_emb_final, users_emb_0 = (
+        users_emb_final[user_indices],
+        users_emb_0[user_indices],
+    )
+    pos_items_emb_final, pos_items_emb_0 = (
+        items_emb_final[pos_item_indices],
+        items_emb_0[pos_item_indices],
+    )
+    neg_items_emb_final, neg_items_emb_0 = (
+        items_emb_final[neg_item_indices],
+        items_emb_0[neg_item_indices],
+    )
+
+    loss = bpr_loss(
+        users_emb_final,
+        users_emb_0,
+        pos_items_emb_final,
+        pos_items_emb_0,
+        neg_items_emb_final,
+        neg_items_emb_0,
+        lambda_val,
+    ).item()
+
+    recall, precision, ndcg = get_metrics(model, edge_index, exclude_edge_indices, k)
+
+    return loss, recall, precision, ndcg
+
+
+def structured_negative_sampling(
+    edge_index, num_nodes: Optional[int] = None, contains_neg_self_loops: bool = True
+):
+    num_nodes = maybe_num_nodes(edge_index, num_nodes)
+    print(num_nodes)
+
+    row, col = edge_index.cpu()
+
+    pos_idx = row * num_nodes + col
+    if not contains_neg_self_loops:
+        loop_idx = torch.arange(num_nodes) * (num_nodes + 1)
+        pos_idx = torch.cat([pos_idx, loop_idx], dim=0)
+
+    # Get the unique nodes from the columns of edge_index
+    unique_nodes = torch.unique(edge_index[1]).cpu()
+
+    # Generate random indices for negative edges from unique_nodes
+    rand = torch.randint(len(unique_nodes), (row.size(0),), dtype=torch.long)
+    neg_idx = row * num_nodes + unique_nodes[rand]
+
+    mask = torch.from_numpy(np.isin(neg_idx, pos_idx)).to(torch.bool)
+    rest = mask.nonzero(as_tuple=False).view(-1)
+
+    while rest.numel() > 0:  # pragma: no cover
+        tmp = torch.randint(len(unique_nodes), (rest.size(0),), dtype=torch.long)
+        rand[rest] = tmp
+        neg_idx = row[rest] * num_nodes + unique_nodes[tmp]
+
+        mask = torch.from_numpy(np.isin(neg_idx, pos_idx)).to(torch.bool)
+        rest = rest[mask]
+
+    return edge_index[0], edge_index[1], unique_nodes[rand].to(edge_index.device)
